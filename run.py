76879587452ebbdb5cf62c6d274769b87a9c523f
tiepvupsu <vuhuutiep@gmail.com>
2019-10-31 16:37:42 -0700

    ? -> '' (#219)

--- run.py
+++ run.py
@@ -16,6 +16,15 @@ PENDING_CHAPTERS = [19]
 CHAPTERS_DIR = './chapters/'
 ALL_CHAPTERS_FILENAME = 'all_chapters.md'
 
+HEADER_TO_LINK_MAP = {' ': '-', '#-': '#'}
+HEADER_TO_LINK_MAP.update({a: '' for a in '.:?/'})
+
+
+def _create_header_link(line):
+    for char, new_char in HEADER_TO_LINK_MAP.items():
+        line = line.replace(char, new_char)
+    return line.lower()
+
 
 def main():
     with codecs.open(os.path.join(CHAPTERS_DIR, ALL_CHAPTERS_FILENAME), 'w', encoding='utf-8') as all_file:
@@ -28,8 +37,9 @@ def main():
             with codecs.open(chapter_path, 'r', encoding='utf-8') as one_file:
                 for line in one_file:
                     if line.startswith('# '):
-                        line = line.strip()
-                        link = line.replace(' ', '-').replace('.', '').replace('#-', '#').replace(':', '').lower()
+                        line=line.strip()
+                        link = _create_header_link(line)
+
                         full_link = "[{display_text}]({link_to_chapter})".format(
                             display_text=line[len('# '):],
                             link_to_chapter=link
