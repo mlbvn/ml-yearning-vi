b0d7baf24f045e59ff9289ddbd7072d61d7c1d8e
Tiep Vu <vuhuutiep@gmail.com>
2019-11-10 23:33:33 -0800

    * cleanup one non-ascii character in ch34.md * python run.py

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from collections import OrderedDict
 # sys.setdefaultencoding('utf8') 
 
 NUM_CHAPTERS = 58
-MAX_CHAPTER = 31
+MAX_CHAPTER = 34
 PENDING_CHAPTERS = []
 
 CHAPTERS_DIR = './chapters/'
