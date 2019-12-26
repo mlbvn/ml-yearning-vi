1a9be3bc460c29ac420af418993b74fdcea34611
Pham Hong Vinh <phamvinh257@gmail.com>
2019-12-26 15:57:48 +0700

    add bat file to create pdf for WindowsOS

---
+++ create_pdf.bat
@@ -0,0 +1,3 @@
+python run.py
+gswin64c -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=machine_learning_yearning.pdf book_cover.pdf all_chapters.pdf acknowledgement.pdf 
+gswin64c -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=machine_learning_yearning_vietnamese.pdf book_cover.pdf all_chapters_vietnamese_only.pdf acknowledgement.pdf  
