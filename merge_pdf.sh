35b2a3152c58cce16d0588e4180de5cb96022e18
rootonchair <phamvinh257@gmail.com>
2019-12-26 15:54:28 +0700

    add bash script for creating pdf

--- merge_pdf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=machine_learning_yearning.pdf book_cover.pdf all_chapters.pdf acknowledgement.pdf
-#gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=machine_learning_yearning_vietnamese.pdf book_cover.pdf all_chapters_vietnamese_only.pdf acknowledgement.pdf 
