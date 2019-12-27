ac8aaf355e4206170d7149563afe984ebe2e16cb
tiepvupsu <vuhuutiep@gmail.com>
2019-12-26 18:04:57 -0800

    Fix missing links while running `mly_run` in linux (#479)
    * add python3 -m grip
    
    * fix missing link in the final pdfs

--- create_pdf.sh
+++ create_pdf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 run.py
-gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=machine_learning_yearning.pdf book_cover.pdf all_chapters.pdf acknowledgement.pdf
-gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=machine_learning_yearning_vietnamese.pdf book_cover.pdf all_chapters_vietnamese_only.pdf acknowledgement.pdf 
+gs -q -dPrinted=false -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=machine_learning_yearning.pdf book_cover.pdf all_chapters.pdf acknowledgement.pdf
+gs -q -dPrinted=false -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=machine_learning_yearning_vietnamese.pdf book_cover.pdf all_chapters_vietnamese_only.pdf acknowledgement.pdf 
