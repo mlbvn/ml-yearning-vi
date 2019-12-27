cb2a0e4fc925b521136dd7c4fa2fb245dd66de81
Tiep Vu <vuhuutiep@gmail.com>
2019-12-27 07:48:48 -0800

    Book: Table of Content

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ ALL_CHAPTERS_FILENAME = 'all_chapters.md'
 ALL_CHAPTERS_VN_FILENAME = 'all_chapters_vietnamese_only.md'
 HEADER_TO_LINK_MAP = OrderedDict([(' ', '-'), ('#-', '#')])
 HEADER_TO_LINK_MAP.update({a: '' for a in '.:?/'})
+BOOK_DIR = './book/'
 
 PARTS = [
     {'path': './chapters/p00_01_04.md', 'range': [1, 4]},
@@ -28,6 +29,101 @@ PARTS = [
     {'path': './chapters/p09_53_57.md', 'range': [53, 57]},
     {'path': './chapters/p10_58.md', 'range': [58, 58]},
 ]
+
+
+class Book(object):
+    def __init__(self):
+        self.en_vi_md_path = self._get_path('myl_en_vi.md')
+        self.vi_md_path = self._get_path('myl_vi.md')
+        self.en_vi_pdf_path = self._get_path('myl_en_vi.pdf')
+        self.vi_pdf_path = self._get_path('myl_vi.pdf')
+
+    @staticmethod
+    def _get_path(filename):
+        return os.path.join(BOOK_DIR, filename)
+
+    def build_all(self):
+        self.build_all_md(vn_only=True)
+        # self.build_all_md(vn_only=False)
+        # self.build_all_pdf(vn_only=True)
+        # self.build_all_pdf(vn_only=False)
+
+    def build_all_md(self, vn_only):
+        output_filename = self.vi_md_path if vn_only else self.en_vi_md_path
+        with codecs.open(output_filename, 'w', encoding='utf-8') as file_writer:
+            # Cover.add_md(file_writer)
+            TableOfContent().add_md(file_writer)
+            # MainContent.add_md(file_writer)
+            # Glossary.add_md(file_writer)
+            # Acknowledge.add_md(file_writer)
+
+    def build_add_pdf(self, vn_only):
+        output_filename = self.vi_pdf_path if vn_only else self.en_vi_pdf_path
+        pass
+
+
+class BookPart(object):
+
+    def _get_content_lines_md(self):
+        """a list of markdown lines to be written, must be implemented in subclasses"""
+        raise NotImplementedError
+
+    def _get_content_lines_html(self):
+        """a list of markdown lines to be written, must be implemented in subclasses"""
+        raise NotImplementedError
+
+    # @property
+    def add_md(self, file_writer):
+        for line in self._get_content_lines_md(): 
+            file_writer.write(line)
+
+    # @property
+    def add_html(self, file_writer):
+        for line in self._get_content_lines_html(): 
+            file_writer.write(line)
+
+
+class TableOfContent(BookPart):
+    def _get_content_lines_md(self):
+        lines = []
+        lines.append("**MỤC LỤC**\n")
+        for part in PARTS:
+            part_path = part['path']
+            lines.append(self.get_toc_line(part_path, level=0))
+            start_chapter, end_chatper = part['range']
+            for chapter_number in range(start_chapter, end_chatper + 1):
+                chapter_path = _chapter_path_from_chapter_number(chapter_number)
+                lines.append(self.get_toc_line(chapter_path, level=1))
+        return lines
+
+    def _get_content_lines_html(self):
+        pass
+
+    @staticmethod
+    def _link_to_part_or_chap(path):
+        filename = os.path.basename(path)
+        
+        # If it is a chapter, created a link syntax with only 2 digits (e.g: #01). 
+        # If it is a part, keep "p" + 2 digit (e.g: #p01)   
+        assert filename[0] in ['c', 'p'], filename
+
+        if filename[0] == 'p':
+            link = "#" + filename[:3]
+        elif filename[0] == 'c':
+            # TODO(tiep): set link to chapter = #chxx instead of #xx
+            link = "#" + filename[2:4]
+        else:
+            assert False, "filename must start with 'c' or 'p', got filename = {}".format(filename)
+        return link
+
+    def get_toc_line(self, part_path, level):
+        part_title = _get_title_from_file_path(part_path)
+        
+        full_link = "[{display_text}]({link_to_chapter})".format(
+            display_text=_remove_sharp(part_title),
+            link_to_chapter=self._link_to_part_or_chap(part_path)
+        )
+        return '\t'*level + '* ' + full_link + '\n'
 
 
 def main(vn_only=True):
@@ -72,31 +168,11 @@ def _get_title_from_file_path(part_path):
 
 def is_part(path_name):
     assert path_name[1] in ['p', 'c'], path_name
-    return path_name[1]=="p"
+    return path_name[1] == "p"
 
 
 def _insert_to_toc(all_file_writer, part_path, level):
-    part_title = _get_title_from_file_path(part_path)
-    full_link = _create_header_link(part_title)
-    
-    # Extract the the path name of each file. For example, ./chapters/ch01.md 
-    # will be trimmed to ch01.md; ./chapters/p00_01_04.md will be trimmed to p00_01_04.md
-    path_name = part_path[part_path.index("s")+1:]
-    
-    
-    # If it is a chapter, created a link syntax with only 2 digits (e.g: #01). 
-    # If it is a part, keep "p" + 2 digit (e.g: #p01)   
-    if is_part(path_name):
-        link = "#" + path_name[1:4]
-    else:
-        link = "#" + path_name[3:5]
-    
-    
-    full_link = "[{display_text}]({link_to_chapter})".format(
-        display_text=_remove_sharp(part_title),
-        link_to_chapter=link
-    )
-    all_file_writer.write('\t'*level + '* ' + full_link + '\n')
+    all_file_writer.write(TableOfContent().get_toc_line(part_path, level))
 
 
 def _insert_content(all_file_writer, file_path, vn_only, heading):
@@ -157,6 +233,7 @@ def create_pdfs():
 
 
 if __name__ == '__main__':
-    main(vn_only=False)
-    main(vn_only=True)
-    create_pdfs()
+    Book().build_all()
+    # main(vn_only=False)
+    # main(vn_only=True)
+    # create_pdfs()
