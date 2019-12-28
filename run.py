e1552a01a7f03de311d0e2c037001148c3f7be5d
Tiep Vu <vuhuutiep@gmail.com>
2019-12-27 17:38:36 -0800

    remove html

--- run.py
+++ run.py
@@ -276,7 +276,7 @@ class BookPDF(object):
         pdfkit.from_file(self.html_file, self.pdf_file, options=options)
 
         # Remove the created html file
-        # os.remove(html_file)
+        os.remove(self.html_file)
 
     def build(self):
         # md to raw html
